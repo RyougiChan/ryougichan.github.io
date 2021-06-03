bf59a0f3725249d885b8ab2de912d27bd4d74626
RyougiChan <ryougi.chan.fire@gmail.com>
2021-06-03 17:18:54 +0800

    burn gpu

--- static/burngpu/module/GLTFLoader.js
+++ static/burngpu/module/GLTFLoader.js
@@ -61,7 +61,7 @@ import {
 	Vector3,
 	VectorKeyframeTrack,
 	sRGBEncoding
-} from '../../../module/three.module.js';
+} from './three.module.js';
 
 class GLTFLoader extends Loader {
 
--- static/burngpu/module/GPUComputationRenderer.js
+++ static/burngpu/module/GPUComputationRenderer.js
@@ -10,7 +10,7 @@ import {
 	Scene,
 	ShaderMaterial,
 	WebGLRenderTarget
-} from '../../../module/three.module.js';
+} from './three.module.js';
 
 /**
  * GPUComputationRenderer, based on SimulationRenderer by zz85
